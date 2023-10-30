9b846613de2d65bf2216b28629fe4a963490bf7c
giulifarias05 <88504587+giulifarias05@users.noreply.github.com>
2023-10-30 16:14:20 -0300

    navbar y footer

---
+++ simondice/src/Components/navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container">
+        <a className="navbar-brand" href="#" >Simón dice...</a>
+        <div className="ml-auto">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">Inicio</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Jugar</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Acerca de</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Contacto</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
